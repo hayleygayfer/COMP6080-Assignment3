adf07e6775deaa5fa8e000354177c48c14b51afa
ollie <oliver.a.hogue@gmail.com>
2021-04-14 14:15:57 +1000

    added login and register, but API request doesn't work :(

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
 function Login () {
-  return (
-    <p>Login is a stub</p>
-  );
+  const sendEmails = () => {
+    // make api request
+    // then copy this into ... register
+    console.log(`login request with ${emailInput} and ${passwordInput}`)
+    // will put this part into a function once i get it working, similar to this post: https://edstem.org/courses/5307/discussion/430203
+    fetch('http://localhost:5005/admin/auth/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      data: {
+        email: emailInput,
+        password: passwordInput
+      }
+    })
+  }
+  const [emailInput, setEmailInput] = React.useState('');
+  const [passwordInput, setPasswordInput] = React.useState('');
+  return <>
+    <h2> Login, Darling </h2>
+    <div>
+      <div>
+        Email:
+        <input
+          type="text"
+          onChange={e => setEmailInput(e.target.value)}
+          value={emailInput}
+        />
+      </div>
+      <div>
+        Password:
+        <input
+          type="password"
+          onChange={e => setPasswordInput(e.target.value)}
+          value={passwordInput}
+        />
+      </div>
+    </div>
+    <button onClick={sendEmails}> Send </button>
+  </>;
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
 
 function Register () {
-  return (
-    <p>Register is a stub</p>
-  );
+  const sendEmails = () => {
+    // make api request
+    // then copy this into ... register
+    console.log(`register request with ${emailInput} and ${passwordInput} and ${nameInput}`)
+    // will put this part into a function once i get it working, similar to this post: https://edstem.org/courses/5307/discussion/430203
+    fetch('http://localhost:5005/admin/auth/register', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      data: {
+        email: emailInput,
+        password: passwordInput,
+        name: nameInput
+      }
+    })
+  }
+  const [emailInput, setEmailInput] = React.useState('');
+  const [passwordInput, setPasswordInput] = React.useState('');
+  const [nameInput, setNameInput] = React.useState('');
+
+  return <>
+    <h2> Register, Mlord </h2>
+    <div>
+      <div>
+        Email:
+        <input
+          type="text"
+          onChange={e => setEmailInput(e.target.value)}
+          value={emailInput}
+        />
+      </div>
+      <div>
+        Password:
+        <input
+          type="password"
+          onChange={e => setPasswordInput(e.target.value)}
+          value={passwordInput}
+        />
+      </div>
+      <div>
+        Name:
+        <input
+          type="text"
+          onChange={e => setNameInput(e.target.value)}
+          value={nameInput}
+        />
+      </div>
+    </div>
+    <button onClick={sendEmails}> Send </button>
+  </>;
 }
 
 export default Register;
