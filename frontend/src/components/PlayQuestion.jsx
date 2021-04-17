86115bacae8f899bf76b8d0e6c7f988b55d6b68b
z5312096 <hayley@gayfer.id.au>
2021-04-17 16:36:30 +1000

    fixed request in playJoin and playQuestion, still has a bugs though

--- frontend/src/components/PlayQuestion.jsx
+++ frontend/src/components/PlayQuestion.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { useHistory } from 'react-router-dom';
+import '../App.css'
+import API from '../api.js';
+const api = new API('http://localhost:5005');
+
 /* STUB */
 function PlayQuestion ({ playID }) {
   // commenting out temporarily to avoid linting errors
-  /*
-  const getQuestion = () => {
-    fetch(`http://localhost:5005/${playID}/question`, {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-      }
-    }).then((data) => {
-      if (data.status === 200) {
-        console.log('Got Question Details');
-        setQuestionData(data.json());
-      }
-    }).catch((error) => {
-      alert('Error: ', error);
-    });
-  };
-
   const [questionData, setQuestionData] = React.useState('');
-  */
+  // const [questionTime, setQuestionTime] = React.useState('');
+
+  const history = useHistory();
+  const getQuestionRequest = async () => {
+    try {
+      const request = await api.makeAPIRequest(`admin/play/join/${playID}`, '', 'GET', '', {});
+      if (request.status === 200) {
+        console.log('Get Question');
+        const data = await request.json();
+        setQuestionData(data.quizQuestionPublicReturn);
+        // setQuestionTime(data.isoTimeLastQuestionStarted);
+        history.push('/play_join');
+      } else throw request.status
+    } catch (error) {
+      alert('Invalid Session');
+    }
+  }
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      getQuestionRequest();
+    }, questionData.timeLimit);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Still have to create answer selection
   return <>
-    Stub
+    { questionData }
   </>
 }
 PlayQuestion.propTypes = {
--- frontend/src/pages/PlayJoin.jsx
+++ frontend/src/pages/PlayJoin.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+// import PlayQuestion from '../components/PlayQuestion'
+import { useHistory } from 'react-router-dom';
+import '../App.css'
+import API from '../api.js';
+const api = new API('http://localhost:5005');
 
 function PlayJoin () {
-  const joinGame = () => {
-    console.log(nameInput)
-    fetch(`http://localhost:5005/play/join/${sessionInput}`, {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      data: {
-        name: nameInput
-      }
-    }).then((data) => {
-      if (data.status === 200) console.log('Joined Game');
-    }).catch((error) => {
-      alert('Error: ', error);
-    });
-  };
-
   const [sessionInput, setSessionInput] = React.useState('');
   const [nameInput, setNameInput] = React.useState('');
+
+  const history = useHistory();
+  const joinGameRequest = async () => {
+    console.log(sessionInput);
+    try {
+      const request = await api.makeAPIRequest(`admin/play/join/${sessionInput}`, '', 'POST', '', {
+        name: nameInput,
+      })
+      if (request.status === 200) {
+        console.log('Joined Game');
+        const data = await request.json();
+        console.log(data.playerId);
+        localStorage.setItem('playID', data.playerId);
+        history.push('/play_join');
+      } else throw request.status
+    } catch (error) {
+      setNameInput('');
+      setSessionInput('');
+      alert('Invalid Input');
+    }
+  }
+
   return <>
     <h2>Join Game Session</h2>
     <div>
@@ -41,7 +50,7 @@ function PlayJoin () {
         />
       </div>
     </div>
-    <button onClick={joinGame}> Join </button>
+    <button onClick={joinGameRequest}> Join </button>
   </>
 }
 
